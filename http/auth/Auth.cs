2a3fc258f098fb9018692c3d028293d0300a24ba
Ralph <ralph@hyperluminalgames.com>
2019-02-07 15:32:31 +0000

    updated UnityWebRequest to UnityWebRequestassetBundle

--- http/auth/Auth.cs
+++ http/auth/Auth.cs
@@ -44,14 +44,14 @@ namespace Azure.StorageServices {
 
     public static StorageRequest GetAuthorizedStorageRequestAssetBundle(StorageServiceClient client, string resourcePath = "", Dictionary<string, string> queryParams = null, Dictionary<string, string> headers = null, int contentLength = 0) {
       string requestUrl = RequestUrl(client, queryParams, resourcePath);
-      StorageRequest request = new StorageRequest(UnityWebRequest.GetAssetBundle(requestUrl));
+      StorageRequest request = new StorageRequest(UnityWebRequestAssetBundle.GetAssetBundle(requestUrl));
       request.AuthorizeRequest(client, Method.GET, resourcePath, queryParams, headers, contentLength);
       return request;
     }
 
     public static StorageRequest GetAuthorizedStorageRequestAssetBundle(StorageServiceClient client, string resourcePath, Hash128 hash, uint crc, Dictionary<string, string> queryParams = null, Dictionary<string, string> headers = null, int contentLength = 0) {
       string requestUrl = RequestUrl(client, queryParams, resourcePath);
-      StorageRequest request = new StorageRequest(UnityWebRequest.GetAssetBundle(requestUrl, hash, crc));
+      StorageRequest request = new StorageRequest(UnityWebRequestAssetBundle.GetAssetBundle(requestUrl, hash, crc));
       request.AuthorizeRequest(client, Method.GET, resourcePath, queryParams, headers, contentLength);
       return request;
     }
